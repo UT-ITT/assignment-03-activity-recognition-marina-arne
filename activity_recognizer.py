d0477dc7e1e4af0fb7d8b41be0a3571bfcf6f521
m-banti <marina.banti@student.uni-tuebingen.de>
2026-05-15 22:22:54 +0200

    add: first test set iteration

--- activity_recognizer.py
+++ activity_recognizer.py
@@ -1,13 +1,14 @@
 # this program recognizes activities
-import activity_recognizer as activity
 import matplotlib
 from matplotlib import pyplot as plt
 from sklearn import svm # scikit-learn
 import seaborn as sns # for nice visualizations
 import pandas as pd # for loading the data from csv
 import numpy as np
-from sklearn.preprocessing import scale, StandardScaler, MinMaxScaler
+from sklearn.preprocessing import scale, StandardScaler
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import accuracy_score, classification_report
 from sklearn import svm
 import glob 
 import joblib
@@ -26,6 +27,7 @@ import argparse
 # 7. visualize fitness trainer with pyglet
 
 parser = argparse.ArgumentParser()
+# TODO exchange here the default path if you (tutors) want to use exclusevely your dataset 
 parser.add_argument("--data", type=str, default="./dataset")
 args = parser.parse_args()
 
@@ -42,6 +44,8 @@ class_dict = {"rowing": 0,
               "jumpingjacks": 3}
 
 col_structure = ['id', 'timestamp', 'acc_x', 'acc_y', 'acc_z', 'gyro_x', 'gyro_y', 'gyro_z']
+sensor_col = ['acc_x', 'acc_y', 'acc_z', 'gyro_x', 'gyro_y', 'gyro_z']
+
 
 for file_path in dataset_csv:
     df = pd.read_csv(file_path)
@@ -50,6 +54,12 @@ for file_path in dataset_csv:
     df_cleanup = df[keep_indices]
     # checking for rows with 0 and columns with 1
     df_cleaned = df_cleanup.dropna(axis=0)
+    # filtering of noise
+    # only filter if columns are available
+    sensor_check = [col for col in sensor_col if col in df_cleaned.columns]
+    if sensor_check:
+        # take 5 values combined and create average for signal processing 
+        df_cleaned[sensor_col] = df_cleaned[sensor_check].rolling(window=5, min_periods=1).mean()
     # get exercise from filename and row with corresponding classification
     file_name = os.path.basename(file_path)
     name_cut = file_name.replace(".csv", "")
@@ -71,20 +81,57 @@ train_set, test_set = train_test_split(df_collection, test_size=0.30, random_sta
 
 train_set.to_csv("train_set.csv", index=False)
 test_set.to_csv("test_set.csv", index=False)
-print("finally the test dataset and training dataset was created! Now lets do some training!")
+print("finally the test dataset and training dataset was created!")
 
-"""
-# mean removal train data
+print("Now lets do some training!")
+# train data!
+# mean removal
 train_data = pd.read_csv("train_set.csv")
+
+sensor_data = ['acc_x', 'acc_y', 'acc_z', 'gyro_x', 'gyro_y', 'gyro_z']
+
+x_train = train_data[sensor_data]
+y_train = train_data['classification']
+
+# pipeline contains scaler and svm
+clf_pipeline = Pipeline([
+    ("scaler", StandardScaler()),
+    ("svm", SVC(kernel='rbf', C=1.0))
+])
+
+#runs first scaler than combines with svm
+clf_pipeline.fit(x_train, y_train)
 
 scaler = StandardScaler()
 scaled_samples = scaler.fit_transform(train_data[['acc_x', 'acc_y', 'acc_z', 'gyro_x', 'gyro_y', 'gyro_z']])
 
+joblib.dump(clf_pipeline, 'svm_pipeline.pkl')
+
+print("We prepared the Model some meal (data) and fed it. Now lets test if we actually fed it right")
+
+test_data = pd.read_csv("test_set.csv")
+features = ['acc_x', 'acc_y', 'acc_z', 'gyro_x', 'gyro_y', 'gyro_z']
+
+x_test = test_data[features]
+y_test = test_data['classification']
+
+pipeline = joblib.load('svm_pipeline.pkl')
+
+# prediction
+y_pred = pipeline.predict(x_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+print(f"and the accuracy is *drumroll* {accuracy * 100:.2f}%")
+
+print("\nreport:")
+print(classification_report(y_test, y_pred, target_names=["rowing", "running", "lifting", "jumpingjacks"]))
+
+"""
 train_mean = train_data.copy()
 train_mean[['acc_x', 'acc_y', 'acc_z', 'gyro_x', 'gyro_y', 'gyro_z']] = scaled_samples
 # minmax scaler
-mms = MinMaxScaler()
-mms.fit(train_mean[['acc_x', 'acc_y', 'acc_z', 'gyro_x', 'gyro_y', 'gyro_z']])
+# mms = MinMaxScaler()
+# mms.fit(train_mean[['acc_x', 'acc_y', 'acc_z', 'gyro_x', 'gyro_y', 'gyro_z']])
 
 # normalized
 scaled_samples = mms.transform(train_mean[['acc_x', 'acc_y', 'acc_z', 'gyro_x', 'gyro_y', 'gyro_z']])
@@ -101,5 +148,4 @@ clf.fit(x_train, y_train)
 joblib.dump(clf, 'svm_model.pkl')
 joblib.dump(scaler, 'std_scaler.pkl')
 joblib.dump(mms, 'minmax_scaler.pkl')
-
 """
