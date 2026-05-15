8194d1a26ac5d2fb93886d9cb3d9a309770fff9e
m-banti <marina.banti@student.uni-tuebingen.de>
2026-05-15 14:55:01 +0200

    feat: preparing and splitting data

--- fitness_trainer.py
+++ fitness_trainer.py
@@ -8,7 +8,10 @@ import seaborn as sns # for nice visualizations
 import pandas as pd # for loading the data from csv
 import numpy as np
 from sklearn.preprocessing import scale, StandardScaler, MinMaxScaler
+from sklearn.model_selection import train_test_split
 from sklearn import svm
+import glob 
+import os
 
 #TODO's
 # 1. import all training data (jumping, rowing, running, lifting)
@@ -20,3 +23,34 @@ from sklearn import svm
 # run continuously without requiring further intervention by the user. 
 # 7. visualize fitness trainer with pyglet
 
+path = "./dataset"
+
+join_csv = glob.glob(os.path.join(path, "*.csv"))
+
+# prepare klarazettler data (contains twice id)
+
+# clear empty rows with missing data for all
+
+df_list = []
+
+# go through every file, take the exercise from filename and add as new column classification
+for file_path in join_csv:
+    file_name = os.path.basename(file_path)
+    name_cut = file_name.replace(".csv", "")
+    name_split = name_cut.split("-")
+    exercise =name_split[1]
+    df_temp = pd.read_csv(file_path)
+    df_temp["classification"] = exercise
+    df_list.append(df_temp)
+
+#df_list = [pd.read_csv(file) for file in join_csv]
+df_collection = pd.concat(df_list, ignore_index=True)
+# randomize before splitting dataset into to 70% training, 30% test 
+train_set, test_set = train_test_split(df_collection, test_size=0.30, random_state=42)
+
+train_set.to_csv("train_set.csv", index=False)
+test_set.to_csv("test_set.csv", index=False)
+
+print(f"Training Set (70%): {len(train_set)}")
+print(f"Test Set (30%): {len(test_set)}")
+print(df_collection["classification"].value_counts())
